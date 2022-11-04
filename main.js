93dfc8127c4acffc3ab120c636726cbb77ddd8f6
TomekWebDev <tommaso.mangiagalli@gmail.com>
2022-11-04 17:11:09 +0100

    input to push new member in the array is working!!

--- main.js
+++ main.js
@@ -31,20 +31,48 @@ let team = [
       role: 'Graphic Designer',
       image: 'barbara-ramos-graphic-designer.jpg',
     },
-  ];
+];
 
 for(i = 0; i < team.length; i++){
     let thisObject = team[i]
 
     document.getElementById("team-members").innerHTML += 
-
-
-`   <div id="card" class="card mx-2 my-2" style="width: 18rem;">
-        <img src="img/${thisObject.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${thisObject.name}</h5>
-            <p class="card-text">${thisObject.role}</p>
-        </div>
-    </div>`
+    `   <div id="card" class="card mx-2 my-2" style="width: 18rem;">
+            <img src="img/${thisObject.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${thisObject.name}</h5>
+                <p class="card-text">${thisObject.role}</p>
+            </div>
+        </div>  ` //attenzione ai backtic!!
   
+}
+
+function registerNewMember(){
+
+    let thisName = document.getElementById("name").value;
+    let thisSurname = document.getElementById("surname").value;
+    let thisRole = document.getElementById("role").value;
+    let thisImage = document.getElementById("memberImage").value;
+
+    let thisObject = {
+        name: `${thisName + " " + thisSurname}`,
+        role: `${thisRole}`,
+        image: `${thisImage}`
+    };
+
+    team.push(thisObject);
+
+    console.log(team);
+
+    document.getElementById("team-members").innerHTML += 
+    `   <div id="card" class="card mx-2 my-2" style="width: 18rem;">
+            <img src="img/${thisObject.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${thisObject.name}</h5>
+                <p class="card-text">${thisObject.role}</p>
+            </div>
+        </div>  ` //attenzione ai backtic!!
+
+    
+
 }
